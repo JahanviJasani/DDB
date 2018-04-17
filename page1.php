97f501a260f3b0c13bb7231e1c114b847244f15e
JahanviJasani <jasanijahanvi@gmail.com>
2018-04-17 18:15:29 +0530

    Added the add offence page

---
+++ page1.php
@@ -0,0 +1,182 @@
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta tags -->
+	<title>General Application Form a Flat Responsive Widget Template :: w3layouts</title>
+	<meta name="keywords" content="General Application Form Responsive widget, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" href="css/style2.css" type="text/css" media="all">
+	
+	<!-- google fonts  -->
+	<link href="//fonts.googleapis.com/css?family=Alegreya+Sans:100,100i,300,300i,400,400i,500,500i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+
+</head>
+<body>
+	<div class="w3ls-banner">
+	<div class="heading">
+		<h1>General Application Form</h1>
+	</div>
+		<div class="container">
+			<div class="heading">
+				<h2>Please Enter Offence Details</h2>
+				<p>Fill the form given below and submit.</p>
+			</div>
+			<div class="agile-form">
+				<form action="#" method="post">
+					<ul class="field-list">
+						<li> 
+							<label class="form-label"> Registration Number <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<input type="text" name="reg_no" placeholder="Vehicle Registration Number" required >
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> City <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="city" required>
+									<option value="">Select City</option>
+									<option value="United states"> United states </option>
+									<option value="Afghanisthan"> Afghanistan </option>
+									<option value="China"> China </option>
+									<option value="Indonesia"> Indonesia </option>
+									<option value="England"> England </option>
+									<option value="Others"> Others </option>
+								</select>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label"> RTO Code <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<input type="text" name="rto" placeholder="RTO Code" required >
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Date of Offence <span class="form-required"> * </span></label>
+							<div class="form-input dob">
+								<span class="form-sub-label">
+									<select name="day" class="day" required>
+										<option>Day</option>
+										<option value="1"> 1 </option>
+										<option value="2"> 2 </option>
+										<option value="3"> 3 </option>
+										<option value="4"> 4 </option>
+										<option value="5"> 5 </option>
+										<option value="6"> 6 </option>
+										<option value="7"> 7 </option>
+										<option value="8"> 8 </option>
+										<option value="9"> 9 </option>
+										<option value="10"> 10 </option>
+										<option value="11"> 11 </option>
+										<option value="12"> 12 </option>
+										<option value="13"> 13 </option>
+										<option value="14"> 14 </option>
+										<option value="15"> 15 </option>
+										<option value="16"> 16 </option>
+										<option value="17"> 17 </option>
+										<option value="18"> 18 </option>
+										<option value="19"> 19 </option>
+										<option value="20"> 20 </option>
+										<option value="21"> 21 </option>
+										<option value="22"> 22 </option>
+										<option value="23"> 23 </option>
+										<option value="24"> 24 </option>
+										<option value="25"> 25 </option>
+										<option value="26"> 26 </option>
+										<option value="27"> 27 </option>
+										<option value="28"> 28 </option>
+										<option value="29"> 29 </option>
+										<option value="30"> 30 </option>
+										<option value="31"> 31 </option>
+									</select>
+								</span>
+								<span class="form-sub-label">
+									<select name="month" required>
+										<option>Month</option>
+										<option value="January"> January </option>
+										<option value="February"> February </option>
+										<option value="March"> March </option>
+										<option value="April"> April </option>
+										<option value="May"> May </option>
+										<option value="June"> June </option>
+										<option value="July"> July </option>
+										<option value="August"> August </option>
+										<option value="September"> September </option>
+										<option value="October"> October </option>
+										<option value="November"> November </option>
+										<option value="December"> December </option>
+									 </select>
+								</span>
+								<span class="form-sub-label">
+									<select name="year" required>
+										<option>Year</option>
+										<option value="1990"> 1990 </option>
+										<option value="1990"> 1991 </option>
+										<option value="1990"> 1992 </option>
+										<option value="1990"> 1993 </option>
+										<option value="1990"> 1994 </option>
+										<option value="1990"> 1995 </option>
+										<option value="1990"> 1996 </option>
+										<option value="1990"> 1997 </option>
+										<option value="1990"> 1998 </option>
+										<option value="1990"> 1999 </option>
+										<option value="1990"> 2000 </option>
+										<option value="1990"> 2001 </option>
+										<option value="1990"> 2002 </option>
+										<option value="1990"> 2003 </option>
+										<option value="1990"> 2004 </option>
+										<option value="1990"> 2005 </option>
+										<option value="1990"> 2006 </option>
+										<option value="1990"> 2007 </option>
+										<option value="1990"> 2008 </option>
+										<option value="1990"> 2009 </option>
+										<option value="1990"> 2010 </option>
+										<option value="1990"> 2011 </option>
+										<option value="1990"> 2012 </option>
+										<option value="1990"> 2013 </option>
+										<option value="1990"> 2014 </option>
+										<option value="1990"> 2015 </option>
+										<option value="1990"> 2016 </option>
+										<option value="1990"> 2017 </option>
+										<option value="1990"> 2018 </option>
+									 </select>
+								</span>
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Offence Type <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="off_cat" required>
+									<option value="">Select the Offence Type</option>
+									<option value="United states"> United states </option>
+									<option value="Afghanisthan"> Afghanistan </option>
+									<option value="China"> China </option>
+									<option value="Indonesia"> Indonesia </option>
+									<option value="England"> England </option>
+									<option value="Others"> Others </option>
+								</select>
+							</div>
+						</li>
+					</ul>
+					<div class="submit_btn">
+						<input type="submit" value="Submit">
+					</div>
+				</form>	
+			</div>
+		</div>
+		<div class="copyright">
+			<p>© 2018 General Application Form. All rights reserved | Design by <a href="www.w3layouts.com">W3layouts</a></p> 
+		</div>
+	</div>
+</body>
+</html>
